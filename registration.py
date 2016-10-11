182a2134c88e08f1e11556a9e4c03d3b90a226df
Rodrigo Ruiz <rodrigoruiz44@yahoo.com>
2016-10-11 00:55:00 -0400

    finished registration

---
+++ registration.py
@@ -0,0 +1,7 @@
+import requests
+
+endpoint = "http://challenge.code2040.org/api/register"
+token = "068a1d291aee44a97349a49bf7bef655"
+github = "https://github.com/zuirod/tech-assessment"
+
+request = requests.post(endpoint, data = dict(token = token, github = github))
