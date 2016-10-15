6db0dae63d21b3935bb2b6b350706322e63314d2
Rodrigo Ruiz <rodrigoruiz44@yahoo.com>
2016-10-15 02:46:45 -0400

    finished prefix challenge

---
+++ prefix.py
@@ -0,0 +1,14 @@
+import requests
+
+token = "068a1d291aee44a97349a49bf7bef655"
+
+data_endpoint = "http://challenge.code2040.org/api/prefix"
+validation_endpoint = "http://challenge.code2040.org/api/prefix/validate"
+
+r = requests.post(data_endpoint, dict(token=token))
+data = r.json()
+prefix = data["prefix"]
+array = data["array"]
+
+strings = [s for s in array if not s.startswith(prefix)]
+answer = requests.post(validation_endpoint, json=dict(token=token, array=strings))
